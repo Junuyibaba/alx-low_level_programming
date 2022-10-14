ef40f4cde67f523d9d3167b77cc4c1460d18f8fa
Junuyibaba <junuyibaba@gmail.com>
2022-10-14 13:04:58 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,12 @@
 
 /* more headers goes there */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 /* betty style doc for function main goes there */
 
 int main(void)
@@ -23,16 +29,16 @@ n = rand() - RAND_MAX / 2;
 
 if (n > 0)
 {
-  printf("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 
-else if(n < 0)
+else if (n < 0)
 {
-  printf("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
- else
-   printf("%d is zero\n", n);
-
+else
+{
+printf("%d is zero\n", n);
+}
 return (0);
-  
 }
