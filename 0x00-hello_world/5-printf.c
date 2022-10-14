90789853cbe8d382e2ddba3e0d72dea0260befbb
Junuyibaba <junuyibaba@gmail.com>
2022-10-13 21:35:42 -0700

    C-files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Start of code
+ *
+ * Return: (0) to end code
+ */
+
+
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
