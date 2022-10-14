3c3120ca3adc8b06b8852682c0e67dae7953a2a3
Junuyibaba <junuyibaba@gmail.com>
2022-10-13 20:38:40 -0700

    C-file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *Use put instead of printf
+ *
+ *return zero ends the code
+ */
+
+int main(void) {
+
+  puts("Programming is like building a multilingual puzzle")
+
+  return (0)
+
+    }  
