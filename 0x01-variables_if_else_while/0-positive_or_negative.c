339a32e4de42f69511db221ff6aae96283e5db05
Junuyibaba <junuyibaba@gmail.com>
2022-10-14 12:38:31 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,9 +20,17 @@ n = rand() - RAND_MAX / 2;
 
 
 if (n>0)
-   return(n);
-else
-   return(0);
+{
+  printf("%d is positive\n",n);
+}
+
+else if(n<0)
+{
+  printf("%d is negative\n",n);
+}
+ else
+   printf("%d is zero\n",n);
+
 return (0);
   
 }
