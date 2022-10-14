ca99efb789bb3a08c7d7145c6555f22ba56b84bb
Junuyibaba <junuyibaba@gmail.com>
2022-10-13 22:04:46 -0700

    gggggggC-filge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 
-/**
- *main - start of code
- *
- *return (0) - ends the code
- */
 
-int main(void) /*start code*/
+/**
+ *main - Entry point
+ *
+ *Return: Always 0(SUccess)
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
