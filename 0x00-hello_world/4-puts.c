63fb295698f496cd0fb0c8819767d01b010a4107
Junuyibaba <junuyibaba@gmail.com>
2022-10-13 21:17:19 -0700

    C-file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,11 @@
  *return zero ends the code
  */
 
-int main(void) {
+int main(void)
+{
+  
+puts("\"Programming is like building a multilingual puzzle");
 
-  puts("\"Programming is like building a multilingual puzzle");
+return (0);
 
-  return (0);
-
-    }  
+}  
