d6df53425cd9bb04aeb80d9c2856a6686def6b04
Junuyibaba <junuyibaba@gmail.com>
2022-10-18 18:48:17 -0700

    check

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
 * Return: Always 0.
 */
 
-int main(void)
+void print_alphabet_x10(void);
 {
 
 int num, i;
- for (num = 0; num <= 9; num++)
+for (num = 0; num <= 9; num++)
 {
-for (i = 'a'; i <= 'z', i++)
+for (i = 'a'; i <= 'z'; i++)
 {
-  _putchar(i);
+_putchar(i);
 }
 
- _putchar('\n');
+_putchar('\n');
 }
 
 return (0)
