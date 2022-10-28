250f79c0550d275d74f54040043a5221f81db18c
Junuyibaba <junuyibaba@gmail.com>
2022-10-27 20:43:48 -0700

    Junuyi

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,14 +8,21 @@
 
 
 char *cap_string(char *s)
+
 {
+
 int count = 0, i;
-int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125}
+int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+  
 
 if (*(s + count) >= 97 && *(s + count) <= 122)
+
 *(s + count) = *(s + count) - 32;
+
 count++;
+
 while (*(s + count) != '\0')
+
 {
 
 for (i = 0; i < 13; i++)
@@ -23,9 +30,11 @@ for (i = 0; i < 13; i++)
 {
 
 if (*(s + count) == separators[i])
+
 {
 
 if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+
 *(s + (count + 1)) = *(s + (count + 1)) - 32;
 
 break;
@@ -37,6 +46,7 @@ break;
 count++;
 
 }
+
 return (s);
 
 }
