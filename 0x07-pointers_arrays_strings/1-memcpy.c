ed121263258db444dcf126313f191f3b9fc32f16
Junuyibaba <junuyibaba@gmail.com>
2022-10-31 21:50:27 -0700

    Junuyi

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,4 @@
 #include "main.h"
-
-
-
 /**
 * *_memcpy - copies memory area
 * @dest: memory area
@@ -10,20 +7,14 @@
 *
 * Return: pointer to dest.
 */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
-
 unsigned int i;
 
-
-
 for (i = 0; i < n; i++)
-
 {
 dest[i] = src[i];
-
 }
 
 return (dest);
