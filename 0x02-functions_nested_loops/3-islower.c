d207ce0638765f50bc137a9b03cf8bf558f78388
Junuyibaba <junuyibaba@gmail.com>
2022-11-23 05:57:05 -0800

    grit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _islower - a program thaat checks for lowercase character.
+* @object: number or an alphabet
+* Return: (0) on success .else error
+*/
+
+int _islower(int object)
+
+{
+if (object >= 'a' && object <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
